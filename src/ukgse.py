56e2a3a6345f8b5a0ce4b21bd1128229c88d7dbf
ShihanYang <dr.yangsh@gmail.com>
2020-10-22 10:45:28 +0800

    add and fix source files

--- src/ukgse.py
+++ src/ukgse.py
@@ -10,7 +10,7 @@
 @Functions: 
     1. This is the main function of the ukgse
     2. Notes: a run command example as following
-          python ukgse.py -
+          python ukgse.py -ds cn15k -dim 128 -batch 128 -epo 200
 ================================================================================
 """
 
@@ -19,9 +19,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # only ERROR messages
 import argparse
 
 parser = argparse.ArgumentParser(description='Predict confidence of Uncertain Knowledge Graph Embedding')
-parser.add_argument('-ds', '--dataset', type=str, help='testing dataset is PPI5k or CN15K')
-parser.add_argument('-s', '--step', type=str, help='there are two steps: one or two')
-
+parser.add_argument('-ds', '--dataset', type=str, default='ppi5k', help='testing dataset is PPI5k or CN15K')
+parser.add_argument('-s', '--step', type=str, default='one', help='there are two steps: one or two')
+parser.add_argument('-dim', '--dimension', type=int, default=128, help='the dimension of embedding vectors')
+parser.add_argument('-batch', '--batchsize', type=int, default=128, help='the batch size when training neural networks')
+parser.add_argument('-epo', '--epochs', type=int, default=200, help='the maximus epochs of training')
 args = parser.parse_args()
 
 if __name__ == '__main__':
